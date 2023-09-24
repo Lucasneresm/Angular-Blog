9e2619cd875cdeaeeab757f7aa2fedbc18c6df60
lucas neres mota <lucasneresm@hotmail.com>
2023-09-23 21:00:27 -0300

    components dinamycs

--- src/app/components/menu-title/menu-title.component.ts
+++ src/app/components/menu-title/menu-title.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuTitleComponent implements OnInit {
 
+  photoCover:string = "https://cultura.uol.com.br/upload/tvcultura/esporte/20211104143637_93218785-596196910973712-1452765875970685902-n-1-1-.jpg"
+  cardTitle:string ="Novo estadio quase pronto !!!"
+  cardDescription:string ="Cofres cheios para usar em transferencia !!"
+
   constructor() { }
 
   ngOnInit(): void {
