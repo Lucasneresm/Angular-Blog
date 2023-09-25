8a42e1f9f27ad7f6f2fe8752336110ff51975002
lucas neres mota <lucasneresm@hotmail.com>
2023-09-25 19:03:12 -0300

    content created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContentComponent } from './pages/content/content.component';
+
 
 const routes: Routes = [];
+
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
